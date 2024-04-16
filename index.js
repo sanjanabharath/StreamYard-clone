a3321ece764361f1debacf480b6dc67a22f8be00
sanjanabharath <sanjanabarath@gmail.com>
2024-04-16 10:48:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import http from 'http'
+import express from 'express'
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(3000, () => console.log("Server is running.."))
